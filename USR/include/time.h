34d1837a3fb3d37a24ed7b9d3db928f17a8dceb0
Bishopjack <97348482+Bishopjack@users.noreply.github.com>
2024-05-02 17:54:10 +0800

    add clock config (#3)

---
+++ USR/include/time.h
@@ -0,0 +1,8 @@
+#ifndef _TIME_H
+#define _TIME_H
+namespace time_conf
+{
+  extern void CLK_Config();
+}
+
+#endif //!_TIME_H
--- USR/main.cpp
+++ USR/main.cpp
@@ -1,7 +1,9 @@
 #include"stm32f10x_it.h"
 #include"motor.h"
+#include"time.h"
 int main(int argc,char** argv)
 {
+	time_conf::CLK_Config();
 	Athletic::motor test(Athletic::motor::GPIO_A,GPIO_Pin_0);
 	return 0;
 }
